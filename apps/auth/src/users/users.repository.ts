27b25a054d30f0628707ed78430fc7005c0a607b
Tran Minh Nghia <nghia.minh@mobifone.vn>
2024-07-04 15:07:25 +0700

    ✨ Add auth service for Hello World! functionality and update dependencies.

---
+++ apps/auth/src/users/users.repository.ts
@@ -0,0 +1,16 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { AbstractRepository } from '@app/common';
+import { UserDocument } from './models/users.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+@Injectable()
+export class UsersRepository extends AbstractRepository<UserDocument> {
+  protected readonly logger = new Logger(UsersRepository.name);
+  constructor(
+    @InjectModel(UserDocument.name)
+    usermodel: Model<UserDocument>,
+  ) {
+    super(usermodel);
+  }
+}

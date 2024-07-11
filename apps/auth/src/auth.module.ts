4fc123127235a1015f80b16834689a6d43cc83d2
Nghia Tran Minh <nghia.minh@mobifone.vn>
2024-07-11 16:44:47 +0700

    ✨ Add typescript definitions for bcryptjs 📦

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -6,6 +6,7 @@ import { LoggerModule } from '@app/common';
 import { JwtModule } from '@nestjs/jwt';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import * as Joi from 'joi';
+import { LocalStategy } from './strategies/local.strategy';
 
 @Module({
   imports: [
@@ -29,6 +30,6 @@ import * as Joi from 'joi';
     }),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, LocalStategy],
 })
 export class AuthModule {}

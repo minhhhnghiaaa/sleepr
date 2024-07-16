1a4a571a360fc655108efa7be45bf40c633f7477
Nghia Tran Minh <nghia.minh@mobifone.vn>
2024-07-16 15:23:31 +0700

    stripe setup

---
+++ apps/payments/src/payments.controller.ts
@@ -0,0 +1,14 @@
+import { Controller, Get } from '@nestjs/common';
+import { PaymentsService } from './payments.service';
+import { MessagePattern, Payload } from '@nestjs/microservices';
+import { CreateChargeDto } from './dto/create-charge.dto';
+
+@Controller()
+export class PaymentsController {
+  constructor(private readonly paymentsService: PaymentsService) {}
+
+  @MessagePattern('create_change')
+  async createChange(@Payload() data: CreateChargeDto) {
+    return this.paymentsService.createChange(data);
+  }
+}

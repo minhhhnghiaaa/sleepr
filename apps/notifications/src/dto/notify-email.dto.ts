ba7ca1fc11f0e5e86e353644b12765dc00016fd9
Nghia Tran Minh <nghia.minh@mobifone.vn>
2024-07-23 15:00:15 +0700

    ✨ Add nodemailer and text field to NotifyEmailDto class.

--- apps/notifications/src/dto/notify-email.dto.ts
+++ apps/notifications/src/dto/notify-email.dto.ts
@@ -1,6 +1,9 @@
-import { IsEmail } from 'class-validator';
+import { IsEmail, IsString } from 'class-validator';
 
 export default class NotifyEmailDto {
   @IsEmail()
   email: string;
+
+  @IsString()
+  text: string;
 }
